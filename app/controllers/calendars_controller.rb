cf70209504e6367afd53dae98bad61ac3ba0f257
yuya-taniguti <yuya2624@gmail.com>
2021-05-03 21:47:31 +0900

    再度修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -35,8 +35,8 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
 
-      wday_num = Date.today.wday + x # wdayメソッドを用いて取得した数値
-      if wday_num >= 7 #「wday_numが7以上の場合」という条件式
+      wday_num = Date.today.wday + x 　# wdayメソッドを用いて取得した数値
+      if wday_num >= 7 　#「wday_numが7以上の場合」という条件式
         wday_num = wday_num -7
       end
 
